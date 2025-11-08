e821ce16d1f4893c3a00e175f5ea8b05e2355eae
Frakcool <frakcool1993@gmail.com>
2025-11-08 14:42:28 -0600

    Initial Commit

---
+++ RideRequestMiniApp/RideRequestMiniAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideRequestMiniAppApp.swift
+//  RideRequestMiniApp
+//
+//  Created by Jesus Sanchez on 08/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RideRequestMiniAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
